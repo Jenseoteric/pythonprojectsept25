8ed1e529657b3e9e5979643b77357c052b522526
Jenson McEwan <jenson.mcewan@students.plymouth.ac.uk>
2025-09-16 05:10:12 +0100

    Updated program, some bugs prevail with looping the spin

--- main.py
+++ main.py
@@ -22,7 +22,20 @@ symbol_value = { #dict invocation
 }
 
 def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
     for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line] # check for negation of the symbol
+            if symbol != symbol_to_check:
+                break
+
+        else:
+            winnings += values[symbol] * bet 
+            winning_lines.append(line + 1)
+
+    return winnings, winning_lines
 
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = [] # list def
@@ -96,16 +109,13 @@ def get_bet():
 
     return amount
     
-
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
         total_bet = bet * lines
 
-        if total_bet >= balance:
+        if total_bet > balance:
             print(f"You do not have enough to bet that amount, your current balance is: ${balance} ")
         else:
             break
@@ -115,6 +125,21 @@ def main():
 
     slots = get_slot_machine_spin(ROWS, COLS, symbol_count)
     print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbol_value)
+    print(f"You won {winnings}.")
+    print(f"You won on lines: ", *winning_lines) #unpack operator
+    return winning_lines - total_bet
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play. (q to quit)")
+        if answer == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
 
 
 main()
